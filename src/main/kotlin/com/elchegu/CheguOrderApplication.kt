592ca588b9eb05b512b481c71ced6b1222eecca1
Francisco Millone <v-fmillone@expedia.com>
2017-03-20 15:38:15 -0300

    base spring boot app

---
+++ src/main/kotlin/com/elchegu/CheguOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.elchegu
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CheguOrderApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CheguOrderApplication::class.java, *args)
+}
